ead066a7fcf1a09144424df09f8c6bc324683b5c
xieyu33333 <xieyu33333@gmail.com>
2013-04-10 15:06:43 +0800

    first commit

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,10 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: MyCtrl1});
+    $routeProvider.when('/product', {templateUrl: 'partials/product.html', controller: MyCtrl2});
+    $routeProvider.when('/download', {templateUrl: 'partials/download.html', controller: MyCtrl3});
+    $routeProvider.when('/connect', {templateUrl: 'partials/connect.html', controller: MyCtrl4});
+    $routeProvider.when('/preorder', {templateUrl: 'partials/preorder.html', controller: MyCtrl5});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,67 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+function MyCtrl1($scope) {
+  $scope.infos = [
+      {"name": "MoStor是什么？",
+       "snippet": "MoStor是一个集成wifi热点，移动电源，移动存储的随身移动工具."},
+      {"name": "无限你的无线",
+       "snippet": "wifi热点功能文案介绍"},
+      {"name": "聪明的数据伴侣",
+       "snippet": "移动存储功能文案介绍"},
+       {"name": "随身移动电源",
+        "snippet": "移动电源功能文案介绍"}
+    ];
 }
-MyCtrl2.$inject = [];
+
+
+
+function MyCtrl2($scope) {
+  $scope.product_infos = [
+      {"name": "无线共享",
+       "snippet": "无线路由功能，支持AP-Client路由模式，具有WIFI信号桥接功能，扩大无线路由的覆盖范围，自带RJ45转接线，支持将WLAN有线网络转换为WIFI信号，支持多台设备同时分享网络资源，适用多种使用环境，兼顾商务人士，现代家庭及旅行者的不同需求。"},
+      {"name": "SSD存储",
+       "snippet": "wifi热点功能文案介绍"},
+      {"name": "NFC",
+       "snippet": "移动存储功能文案介绍"},
+       {"name": "数据安全",
+        "snippet": "移动电源功能文案介绍"}
+    ];
+}
+
+
+function MyCtrl3($scope) {
+  $scope.download_infos = [
+      {"name": "ios版下载",
+       "snippet": "适用于ios5.0以上版本"},
+      {"name": "安卓版下载",
+       "snippet": "适用于andriod2.3以上版本"},
+      {"name": "winphone版下载",
+       "snippet": "适用于windows phone 8版本"}
+    ];
+}
+
+function MyCtrl4($scope) {
+  $scope.connect_infos = [
+      {"name": "联系电话",
+       "snippet": "400-810-95927"},
+      {"name": "联系邮箱",
+       "snippet": "smapp@smapp.hk"},
+      {"name": "新浪微博",
+       "snippet": "@卓衡科技"}
+    ];
+}
+
+function MyCtrl5($scope) {
+  $scope.preorder_infos = [
+      {"name": "32GB",
+       "snippet": "适用于ios5.0以上版本"},
+      {"name": "64GB",
+       "snippet": "适用于andriod2.3以上版本"},
+      {"name": "128GB",
+       "snippet": "适用于windows phone 8版本"}
+    ];
+}
+
+
+
